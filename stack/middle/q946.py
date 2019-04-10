f5a26cf5ae3c1b0a6bcddb9674c7c65c55fcb777
sundazhong <252301699@qq.com>
2019-04-10 14:02:24 +0800

    一题

---
+++ stack/middle/q946.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+# 给定 pushed 和 popped 两个序列，只有当它们可能是在最初空栈上进行的推入 push 和弹出 pop 操作序列的结果时，返回 true；否则，返回 false 。
+
+
+# 示例 1：
+
+# 输入：pushed = [1,2,3,4,5], popped = [4,5,3,2,1]
+# 输出：true
+# 解释：我们可以按以下顺序执行：
+# push(1), push(2), push(3), push(4), pop() -> 4,
+# push(5), pop() -> 5, pop() -> 3, pop() -> 2, pop() -> 1
+
+# 示例 2：
+
+# 输入：pushed = [1,2,3,4,5], popped = [4,3,5,1,2]
+# 输出：false
+# 解释：1 不能在 2 之前弹出。
+
+class Solution(object):
+	def validateStackSequences(self, pushed, popped):
+		"""
+		:type pushed: List[int]
+		:type popped: List[int]
+		:rtype: bool
+		"""
+		a_stack = []
+		pop_index = 0
+		for p in pushed:
+			a_stack.append(p)
+			while a_stack and popped and popped[pop_index] == a_stack[len(a_stack) - 1]:
+				pop_index += 1
+				a_stack.pop()
+
+		return not bool(a_stack)
+
+
+pushed = [1, 2, 3, 4, 5]
+popped = [4, 3, 5, 2, 1]
+a = Solution().validateStackSequences(pushed, popped)
+print(a)
